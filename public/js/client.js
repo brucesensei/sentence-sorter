e74f01e6f630f2705d2595ba403358a602036f19
nolan bruce <nolan_76@msn.com>
2023-03-16 18:57:03 +0900

    Succeeded in creating drag and drop functionality for  one sentence. will now move necessary items into the create function to update the page when next button is pushed.

---
+++ public/js/client.js
@@ -0,0 +1,157 @@
+// --------------VARIABLE DECRLARATION AND CREATION OF LIST OF LISTS FOR VALIDATION--------------------
+
+// Get quiz sentences.
+const sentences = document.getElementsByClassName("sentences");
+
+// get buttons
+const check = document.getElementById("check")
+
+const show = document.getElementById("show")
+
+const next = document.getElementById("next")
+
+// Get the question box
+const questionBox = document.getElementById("question-box-id")
+
+// Get the main-game div to append dynamically created elements to.
+const mainGame = document.getElementById("main-game")
+
+// Array of Arrays. Ex: [["I'm", "fine"], ["This", "is", "a", "pen"]] Correct answers
+const validationArrays = [];
+
+// Current question array
+let randomArray = [];
+
+
+// Declare dragStartIndex.
+let dragStartIndex;
+
+// Get total number of questions for the quiz.
+let totalQuestions = sentences.length;
+document.getElementById("total-questions").innerHTML = totalQuestions;
+
+// Set the question number. (also used to advance to the next question)
+let questionNumber = 0;
+document.getElementById("question-number").innerHTML = questionNumber + 1
+
+// Create the list of lists held in validatoinArray.
+for (i=0; i<sentences.length; i++) {
+  // hold the value of one sentence, change it to lower case and strip off punctuation.
+  let sentenceString = sentences[i]
+  .innerHTML
+  .toLowerCase()
+  .replace(/[.?!]/g, "");
+  // split each sentence into an array of words and append it to validationArrays.
+  let validationArray = sentenceString.split(" ");
+  validationArrays.push(validationArray);
+}
+
+// ---------------------CREATE GAME FOR ONLY ONE LIST AT A TIME------------------------------------------
+
+
+function createList() {
+  [...validationArrays[questionNumber]]
+  .map(a=> ({ value: a, sort: Math.random() }))
+  .sort((a, b) => a.sort - b.sort)
+  .map(a => a.value)
+  .forEach((word, index) => {
+    const listItem = document.createElement('li');
+    listItem.setAttribute('data-index', index);
+    listItem.setAttribute('class', 'word-container')
+    listItem.innerHTML = `
+    <div class="draggable" draggable="true">
+        <p class="sentence-word">${word}</p>
+        </div>
+        `;
+        randomArray.push(listItem);
+        questionBox.appendChild(listItem)
+      });
+      addEventListeners();
+    }
+    
+    function dragStart() {
+      dragStartIndex = +this.closest('li').getAttribute('data-index');
+    }
+    
+    function dragEnter() {
+      this.classList.add('over');
+    }
+    
+    function dragLeave() {
+      this.classList.remove('over');
+    }
+    
+    function dragOver(e) {
+      this.classList.add('over');
+      e.preventDefault();
+    }
+    
+    function dragDrop() {
+      this.classList.remove('over')
+      let dragEndIndex = +this.getAttribute('data-index');
+      swapItems(dragStartIndex, dragEndIndex);
+    }
+
+function swapItems(fromIndex, toIndex) {
+  const itemOne = randomArray[fromIndex].querySelector('.draggable');
+  const itemTwo = randomArray[toIndex].querySelector('.draggable');
+  
+  randomArray[fromIndex].appendChild(itemTwo);
+  randomArray[toIndex].appendChild(itemOne);
+}
+
+function checkOrder() {
+  // listItems are the randomly generated list of fruits with all the bells and whistles attached.
+  randomArray.forEach((listItem, index) => {
+    //get the div with the draggable class and the inner text from the internal p tag trimmed
+    // it also gets the index of each item
+    const word = listItem.querySelector('.draggable').innerText.trim();
+    if (word !== validationArrays[questionNumber][index]) {
+      listItem.classList.add('wrong');
+    } else {
+      listItem.classList.remove('wrong');
+      listItem.classList.add('right');
+    }
+  });
+}
+
+function addEventListeners() {
+  const wordContainers = document.querySelectorAll('.word-container');
+  const draggables = document.querySelectorAll('.draggable');
+  
+  draggables.forEach(draggable => {
+    draggable.addEventListener('dragstart', dragStart);
+  })
+  
+  wordContainers.forEach(container => {
+    container.addEventListener('dragover', dragOver);
+    container.addEventListener('drop', dragDrop);
+    container.addEventListener('dragenter', dragEnter);
+    container.addEventListener('dragleave', dragLeave);
+  })
+}
+
+
+function showAnswer () {
+  questionBox.innerHTML = sentences[questionNumber].innerText
+  console.log(sentences[questionNumber])
+}
+
+function nextQuestion() {
+  questionNumber += 1;
+  if (questionNumber <= totalQuestions) {
+    questionBox.innerHTML = "";
+    createList();
+  } else {
+    questionBox.innerText = "The End"
+  }
+}
+
+
+check.addEventListener('click', checkOrder);
+
+show.addEventListener('click', showAnswer);
+
+next.addEventListener('click', nextQuestion);
+
+createList();
